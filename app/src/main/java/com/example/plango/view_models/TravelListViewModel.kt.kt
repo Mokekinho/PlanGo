f2a0f5be0d5a71f39ec0e59dad02bb3c63a2549c
Moises Protazio <moisesprotazio@gmail.com>
2025-11-23 10:33:46 -0300

    fix: fiz Rotation bug on TravelListScreen

--- app/src/main/java/com/example/plango/view_models/TravelListViewModel.kt.kt
+++ app/src/main/java/com/example/plango/view_models/TravelListViewModel.kt.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.plango.database.TravelRepository
 import com.example.plango.model.Travel
+import com.example.plango.ui.screen.TravelListScreen
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -20,53 +21,27 @@ class TravelListViewModel(
     private val repository: TravelRepository, // this view model will receive just the repository object that holds the access of the DB
 ) : ViewModel() {
 
-    // no tutorial que eu vi sobre view model ele usou uma variavel que era private set, basicamente é isso que as duas fazem, uma serva pra mudar o stado e a outra pra ser observada pela view
-
-    private val _state = MutableStateFlow(TravelListState()) // É onde o ViewModel armazena e modifica o estado interno da tela. O prefixo _ (underscore) é uma convenção para indicar que ela não deve ser acessada ou alterada diretamente de fora do ViewModel.
-    val state: StateFlow<TravelListState> = _state.asStateFlow() //É a variável que a View (sua Activity, Fragment ou Composable) observa. Ela expõe o estado atual, mas não permite que a View o modifique, garantindo que apenas o ViewModel possa fazer alterações.
-
-
-    //MutableStateFlow é um um Flow que  possui um valor inicial (TravelListState()) e que pode ser mudado, ele notifica os receptores somente quando o valor muda para um diferente do anterior.
-
-    // asStateFlow() faz com que essa nova variavel nao seja mudada, apenas vista com .collect()
-
-
-
-    init { // inicia a nossa view model ja caregando os dados
-        observeTravels() //mudar o nome da Função pq agora nós vamos observar as vigems, ou seja se tiver mudança iremos reagir
-    }
-
-
-    fun observeTravels() {
-        viewModelScope.launch { // esse viewModelScope é onde a gente vai lançar nossas coroutinas, isso é util pq nesse caso essa função vai fazer um download, o que é assincrono, além disso, esse escopo faz com que as croutinas sejam cancelas quando nao sao mais uteis, por exemplo, o usuario saiu da tela. O .lauch inicia uma coroutina
-
-            repository.getAllTravels()
-                .onStart { // executa antes de coletar, útil pra mostrar um loading.
-                    _state.update {
-                        it.copy(
-                            isLoading = true,
-                            error = null
-                        )
-                    }
-                }
-                .catch { e -> // pega erros
-                    _state.update {
-                        it.copy(isLoading = false,
-                            error = e.message)
-                    }
-                }
-                .collect { travels-> // ainda se refere ao obejto repository.getAllTravels
-                    //executa toda vez que os dados no banco mudam (inserção, exclusão, atualização).
-                    _state.update {
-                        it.copy(
-                            isLoading = false,
-                            travels = travels,
-                            error = null
-                        )
-                    }
-                }
+    val state: StateFlow<TravelListState> = repository.getAllTravels()
+        .map {
+            TravelListState(
+                isLoading = false,
+                error = null,
+                travels = it
+            )
         }
-    }
+        .onStart {
+            emit(TravelListState(isLoading = true))
+        }
+        .catch { e ->
+            emit(TravelListState(isLoading = false,error = e.message))
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = TravelListState(isLoading = true)
+
+        )
+
 }
 
 class TravelListViewModelFactory( // O propósito dele é dizer ao sistema Android (ou, mais especificamente, à biblioteca ViewModel) como criar uma instância do seu TravelListViewModel quando ele precisa de um argumento, no caso, o TravelRepository.
